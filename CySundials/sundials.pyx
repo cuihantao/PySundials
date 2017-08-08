f78eff1b5ce57b7495e959a3c7280808c9f6aa11
Ryan Pepper <ryan.pepper@soton.ac.uk>
2017-08-08 12:52:16 +0100

    Remove conditional cdef extern as this behaviour is not possible

--- CySundials/sundials.pyx
+++ CySundials/sundials.pyx
@@ -4,10 +4,23 @@ cdef extern from "cython_sundials_config.h":
     int PRECISION
     char * SUNDIALS_PACKAGE_VERSION
 
-version = SUNDIALS_PACKAGE_VERSION.decode('utf-8')
-print(version)
-
 cdef extern from "sundials/sundials_types.h":
     ctypedef double realtype
 
+if PRECISION != 2:
+    raise Exception("Must recompile CySundials - change precision in file sundials.pyx")
 
+
+
+cdef test(realtype a, realtype b):
+    return a + b
+
+
+cdef realtype a, b
+
+a = 2.0
+b = 12
+
+print(test(a, b))
+
+print(SUNDIALS_PACKAGE_VERSION)
